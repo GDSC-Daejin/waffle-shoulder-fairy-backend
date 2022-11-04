38a2f3def8ca5140ed5adeac545537ebc0b6b3b5
jeonsangmin <jsangmin99@gmail.com>
2022-11-04 23:44:28 +0900

    깃 test master

--- src/main/java/com/gdsc/waffle/domain/service/CategoryService.java
+++ src/main/java/com/gdsc/waffle/domain/service/CategoryService.java
@@ -14,7 +14,7 @@ import java.util.Objects;
 @Slf4j
 @Service
 public class CategoryService {
-    @Autowired //DI
+    @Autowired //D  I
     private CategoryRepository categoryRepository;
     public List<CategoryEntity> index(){
         return categoryRepository.findAll();
