8b8de9c44a2d992c3e7f772bee3ee4af40c7b9a7
jeonsangmin <jsangmin99@gmail.com>
2022-12-27 01:10:55 +0900

    불필요한것 정리

--- src/main/java/com/gdsc/waffle/domain/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gdsc.waffle.domain.repository;
-
-import com.gdsc.waffle.domain.entity.STest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<STest, Long> {
-}
